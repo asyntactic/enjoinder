2a4b3020d358a0ed36ba9a2caaca9e1134badea6
unknown <caulkinsal@RMS-13-CAULKINA.cvilleschools.com>
2013-02-28 09:50:58 -0600

    one-many-relation-test

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [zilti/korma.incubator "0.1.1-SNAPSHOT"]
+                 [lobos "1.0.0-beta1"]
+                 [fixdit "1.0.0-SNAPSHOT"]
+                 [ring-mock "0.1.3"]])
--- src/enjoinder/core.clj
+++ src/enjoinder/core.clj
@@ -1,44 +1,61 @@
 (ns enjoinder.core
   (:use [clojure.test]
         [korma.incubator.core :only [select where fields insert delete
-                                     values sql-only]]
-        [korma.db :only [defdb]]
+                                     values sql-only with-object
+                                     order]]
         [fixdit.core :only [load-fixtures]]
         (lobos [connectivity :only [open-global close-global]])
         ring.mock.request))
 
-(defn get-user-id [username]
-  (:id (first (select users
-                      (fields :id)
-                      (where {:username username})))))
+(comment given
+         (deftest users-roles-join
+           (let [u_id (get-user-id "tom")
+                 r_id (get-role-id "manager")
+                 u (first (select users
+                                  (where {:id u_id})
+                                  (with-object roles
+                                    (order :id))))
+                 t (first (select roles
+                                  (where {:id r_id})
+                                  (with-object users)))]
+             (is (= r_id (get-in u [:roles 0 :id])))
+             (is (= u_id (get-in t [:users :id])))))
+         
+         (defn get-user-id [username]
+           (:id (first (select users
+                               (fields :id)
+                               (where {:username username})))))
+         
+         (defn get-role-id [name]
+           (:id (first (select roles
+                               (fields :id)
+                               (where {:name name}))))))
 
-(defn delete-fixtures []
-  (delete users))
-
-(defn create-fixtures []
-  (load-fixtures {:entity-namespace 'namespace.models.models
-                  :yaml-file "resources/fixtures/fixtures.yaml"
-                  :db-type :postgres}))
-
-(defn fixture-func [f]
-  (delete-fixtures)
-  (create-fixtures)
-  (f))
-
-(defn delete-fixture-func [f]
-  (delete-fixtures)
-  (f))
-
-(defn connection-fixture [f]
-  (defdb mfgt-db test-db)
-  (f))
+(defmacro one-many-relation-test [name
+                                  rel1 id-exp1
+                                  rel2 id-exp2]
+  `(deftest ~name
+     (let [id1# ~id-exp1
+           id2# ~id-exp2
+           o1# (first (select ~rel1
+                              (where {:id id1#})
+                              (with-object ~rel2
+                                (order :id))))
+           o23 (first (select ~rel2
+                              (where {:id id2#})
+                              (with-object ~rel1
+                                (order :id))))]
+       (is (= id2# (get-in o1 [~(keyword rel2) 0 :id]))
+           ~(str rel2 " instance in " rel1))
+       (is (= id1# (get-in o2 [~(keyword rel1) :id]))
+           ~(str rel1 " instance in " rel2)))))
 
 (defn response-session-cookie [res]
   (nth (re-matches #"^(.*);Path=/$" 
                    (first (get-in res [:headers "Set-Cookie"])))
        1))
 
-(defn wrap-logged-on-user [username password]
+(defn wrap-logged-on-user [app username password]
   (let [auth (app (-> (request :post "/login")
                       (body {:username username
                              :password password})))
